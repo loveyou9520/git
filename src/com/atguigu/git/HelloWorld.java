86d7b2f2279a6aa682aa8a1774d8e15bbf87ddc1
loveyou9520 <499012573@qq.com>
2016-10-08 19:40:11 +0800

    Update HelloWorld.java
    update

--- src/com/atguigu/git/HelloWorld.java
+++ src/com/atguigu/git/HelloWorld.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello.....");
+		System.out.println("hello.....world");
 	}
 
 }
